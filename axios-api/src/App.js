c6b9190321ce0349923dd110510e1293ca4eeaf4
Nikki Lin <nikkiylin@gmail.com>
2020-03-29 23:37:07 -0700

    initial the project

---
+++ axios-api/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Learn how to call Api with Axios</h1>
+     
+    </div>
+  );
+}
+
+export default App;
